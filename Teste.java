59a9aed151dafc369de0dda6cd4f068407e57be3
Filipe Ferreira <54755667+filipesfbr@users.noreply.github.com>
2020-06-13 08:18:48 -0300

    Add files via upload

--- Teste.java
+++ Teste.java
@@ -4,7 +4,6 @@ import java.io.File;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Scanner;
@@ -18,14 +17,15 @@ public class Teste {
 
 		Programa programa = new Programa();
 		try {
-			programa.carregar(new File("C:/test.csv"));
+			programa.carregar(new File("/Users/filipeferreira/eclipse-workspace/TrabGB/src/AVLPesquisa/ARQUIVO.csv"));
 
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
+		
 
 		System.out.println("Digite uma opção: " + "\n1 - buscar com CPF" + "\n2 - buscar por Nome"
-				+ "\n3 - buscar por Data de Nascimento");
+				+ "\n3 - buscar por Data de Nascimento" + "\n0 - sair do programa");
 		int opcao = ler.nextInt();
 
 		while (opcao != 0) {
@@ -36,11 +36,13 @@ public class Teste {
 				String cpfBuscado = ler.next();
 				No<String, Pessoa> cpf = programa.buscarCpf(cpfBuscado, programa.arvoreCpf);
 
-				if (cpf.getConteudo() != null) {
+				if (cpf != null) {
+					System.out.println("Regitro encontrado: \n");
 					System.out.println(cpf.getConteudo());
 				} else {
 					System.out.println("CPF não encontrado!");
 				}
+				break;
 
 			case 2:
 				System.out.println("Digite o nome a ser buscado: ");
@@ -48,12 +50,14 @@ public class Teste {
 				List<No<String, Pessoa>> nomes = programa.buscarTodosQueIniciamCom(nomeBuscado, programa.arvoreNome);
 
 				if (!nomes.isEmpty()) {
+					System.out.println("Registros encontrados: \n");
 					for (No<String, Pessoa> noNomes : nomes) {
 						System.out.println(noNomes.getConteudo());
 					}
 				} else {
 					System.out.println("Não existem dados para a busca realizada.");
 				}
+				break;
 
 			case 3:
 				System.out.println("Digite 2 datas para uma pesquisa entre elas: ");
@@ -62,13 +66,15 @@ public class Teste {
 			
 				//List<No<Date, Pessoa>> entreDatas = programa.buscarTodosEntreDatas(dataInicio, dataFinal, programa.arvoreData);
 				
+				break;
 				
 			default:
 				System.out.println("Digite apenas opções válidas!");
 			}
 
+			System.out.println("\n");
 			System.out.println("Digite uma opção: " + "\n1 - buscar com CPF" + "\n2 - buscar por Nome"
-					+ "\n3 - buscar por Data de Nascimento");
+					+ "\n3 - buscar por Data de Nascimento" + "\n0 - sair do programa");
 			opcao = ler.nextInt();
 		}
 
@@ -79,17 +85,18 @@ public class Teste {
 		
 		
 		
-		Long segundos = 1000L;
-		Long minutos = 60 * segundos;
-		Long horas = 60 * minutos;
-		Long dias = 24 * horas;
-		Date inicio = new Date(946692000000L); // Jan 01 2000
-		Date fim = new Date(946692000000L + 20 * dias); // Jan 21 2000
-		List<No<Date, Pessoa>> entreDatas = programa.buscarTodosEntreDatas(inicio, fim, programa.arvoreData);
-		System.out.println("Temos " + entreDatas.size() + " elementos entre " + inicio + " e " + fim + ", esperamos 20.");
-		System.out.println("Estas pessoas são: ");
-		for (No<Date, Pessoa> noEntreDatas : entreDatas) {
-			System.out.println(noEntreDatas.getConteudo());
-		}
+//		Long segundos = 1000L;
+//		Long minutos = 60 * segundos;
+//		Long horas = 60 * minutos;
+//		Long dias = 24 * horas;
+//		Date inicio = new Date(946692000000L); // Jan 01 2000
+//		Date fim = new Date(946692000000L + 20 * dias); // Jan 21 2000
+//		
+//		List<No<Date, Pessoa>> entreDatas = programa.buscarTodosEntreDatas(inicio, fim, programa.arvoreData);
+//		System.out.println("Temos " + entreDatas.size() + " elementos entre " + inicio + " e " + fim + ", esperamos 20.");
+//		System.out.println("Estas pessoas são: ");
+//		for (No<Date, Pessoa> noEntreDatas : entreDatas) {
+//			System.out.println(noEntreDatas.getConteudo());
+//		}
 	}
 }
