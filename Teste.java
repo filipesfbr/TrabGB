db1f1e0877f9a13eeeddcd22c9d3017b0685dfce
Filipe Ferreira <54755667+filipesfbr@users.noreply.github.com>
2020-06-12 22:59:56 -0300

    Add files via upload

--- Teste.java
+++ Teste.java
@@ -1,6 +1,8 @@
 package AVLPesquisa;
 
 import java.io.File;
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -8,45 +10,86 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Teste {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 
 		SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
+		Scanner ler = new Scanner(System.in);
+		DateFormat data=DateFormat.getInstance();
+
 		Programa programa = new Programa();
 		try {
-			programa.carregar(new File("C:\\Users\\Lenon\\Desktop\\test.csv"));
+			programa.carregar(new File("C:/test.csv"));
 
 		} catch (Exception exception) {
 			exception.printStackTrace();
 		}
+
+		System.out.println("Digite uma opção: " + "\n1 - buscar com CPF" + "\n2 - buscar por Nome"
+				+ "\n3 - buscar por Data de Nascimento");
+		int opcao = ler.nextInt();
+
+		while (opcao != 0) {
+			switch (opcao) {
+
+			case 1:
+				System.out.println("Digite o CPF que deseja buscar: ");
+				String cpfBuscado = ler.next();
+				No<String, Pessoa> cpf = programa.buscarCpf(cpfBuscado, programa.arvoreCpf);
+
+				if (cpf.getConteudo() != null) {
+					System.out.println(cpf.getConteudo());
+				} else {
+					System.out.println("CPF não encontrado!");
+				}
+
+			case 2:
+				System.out.println("Digite o nome a ser buscado: ");
+				String nomeBuscado = ler.next();
+				List<No<String, Pessoa>> nomes = programa.buscarTodosQueIniciamCom(nomeBuscado, programa.arvoreNome);
+
+				if (!nomes.isEmpty()) {
+					for (No<String, Pessoa> noNomes : nomes) {
+						System.out.println(noNomes.getConteudo());
+					}
+				} else {
+					System.out.println("Não existem dados para a busca realizada.");
+				}
+
+			case 3:
+				System.out.println("Digite 2 datas para uma pesquisa entre elas: ");
+				String dataInicio = ler.next();
+				String dataFinal = ler.next();
+			
+				//List<No<Date, Pessoa>> entreDatas = programa.buscarTodosEntreDatas(dataInicio, dataFinal, programa.arvoreData);
+				
+				
+			default:
+				System.out.println("Digite apenas opções válidas!");
+			}
+
+			System.out.println("Digite uma opção: " + "\n1 - buscar com CPF" + "\n2 - buscar por Nome"
+					+ "\n3 - buscar por Data de Nascimento");
+			opcao = ler.nextInt();
+		}
+
 		
-//		Scanner ler = new Scanner(System.in);
-//		String nome = ler.next();
 		
-		Scanner ler2 = new Scanner(System.in);
-		String cpf = ler2.next();
 		
-//		List<No<String, Pessoa>> nomes = programa.buscarTodosQueIniciamCom(nome, programa.arvoreNome);
-		No<String, Pessoa> cpfs = programa.buscarCpf(cpf, programa.arvoreCpf);
-		System.out.println(cpfs.getConteudo());
+		//verificar o tipo de entrada no método de buscar entre datas
 		
-//		for (No<String, Pessoa> noNomes : nomes) {
-//			System.out.println(noNomes.getConteudo()); 
-//		}
-//		
 		
-//		List<No<String, Pessoa>> iniciamComMar = programa.buscarTodosQueIniciamCom("Mar", programa.arvoreNome);
-//		System.out.println("Procurando por 'Mar', temos " + iniciamComMar.size() + " elementos, esperamos 27500.");
-//
-//		Long segundos = 1000L;
-//		Long minutos = 60 * segundos;
-//		Long horas = 60 * minutos;
-//		Long dias = 24 * horas;
-//		Date inicio = new Date(946692000000L); // Jan 01 2000
-//		Date fim = new Date(946692000000L + 20 * dias); // Jan 21 2000
-//		List<No<Date, Pessoa>> entreDatas = programa.buscarTodosEntreDatas(inicio, fim, programa.arvoreData);
-//		System.out.println("Temos " + entreDatas.size() + " elementos entre " + inicio + " e " + fim + ", esperamos 20.");
-//		System.out.println("Estas pessoas são: ");
-//		for (No<Date, Pessoa> noEntreDatas : entreDatas) {
-//		System.out.println(noEntreDatas.getConteudo()); 
+		
+		Long segundos = 1000L;
+		Long minutos = 60 * segundos;
+		Long horas = 60 * minutos;
+		Long dias = 24 * horas;
+		Date inicio = new Date(946692000000L); // Jan 01 2000
+		Date fim = new Date(946692000000L + 20 * dias); // Jan 21 2000
+		List<No<Date, Pessoa>> entreDatas = programa.buscarTodosEntreDatas(inicio, fim, programa.arvoreData);
+		System.out.println("Temos " + entreDatas.size() + " elementos entre " + inicio + " e " + fim + ", esperamos 20.");
+		System.out.println("Estas pessoas são: ");
+		for (No<Date, Pessoa> noEntreDatas : entreDatas) {
+			System.out.println(noEntreDatas.getConteudo());
 		}
 	}
+}
